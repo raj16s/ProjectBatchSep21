91bf32914e257b20f495cdfa822656e75ccc77fe
raj16s <pmraj16s@gmail.com>
2021-09-25 16:19:16 +0530

    Kutty completed her work

--- src/org/sample/SampleProgram.java
+++ src/org/sample/SampleProgram.java
@@ -2,6 +2,9 @@ package org.sample;
 
 public class SampleProgram {
 
+	private void poi() {
+System.out.println("1234567890");
+	}
 	public static void main(String[] args) {
 		System.out.println("Sample Program");
 	}
